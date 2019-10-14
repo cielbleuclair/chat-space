9efd94ac347e509c5dfd4af4f510ffd0878c7cf5
cielbleuclair <matsunagazm@gmail.com>
2019-10-14 20:03:09 +0900

    Set routing and Define action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,48 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only:[:edit, :update]
+
+  def index
+    @groups = current_user.groups
+  end
+
+  def new
+    @group = Group.new
+    @group.users << current_user
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      respond_to do |format|
+        format.html { redirect_to root_path, notice: "グループを作成しました。" }
+        format.json
+      end
+    else
+      flash[:alert] = "グループ名にデータを入力していないため保存できませんでした。"
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    if @group.update(group_params)
+      redirect_to root_path(@group), notice: "グループを変更しました。"
+    else
+      flash[:alert] = "グループ名にデータを入力していないため保存できませんでした。"
+      render :edit
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, user_ids:[])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
+  end
 end
