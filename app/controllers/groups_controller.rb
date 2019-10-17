d0eac39e2e0d24023ef4921cff2d7cf03922d305
cielbleuclair <matsunagazm@gmail.com>
2019-10-17 17:58:23 +0900

    Implement group creation function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,4 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only:[:edit, :update]
-
-  def index
-    @groups = current_user.groups
-  end
-
   def new
     @group = Group.new
     @group.users << current_user
@@ -13,36 +7,14 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     if @group.save
-      respond_to do |format|
-        format.html { redirect_to root_path, notice: "グループを作成しました。" }
-        format.json
-      end
+      redirect_to root_path, notice: 'グループを作成しました'
     else
-      flash[:alert] = "グループ名にデータを入力していないため保存できませんでした。"
       render :new
     end
   end
 
-  def edit
-    @group = Group.find(params[:id])
-  end
-
-  def update
-    if @group.update(group_params)
-      redirect_to root_path(@group), notice: "グループを変更しました。"
-    else
-      flash[:alert] = "グループ名にデータを入力していないため保存できませんでした。"
-      render :edit
-    end
-  end
-
   private
-
   def group_params
-    params.require(:group).permit(:name, user_ids:[])
-  end
-
-  def set_group
-    @group = Group.find(params[:id])
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
